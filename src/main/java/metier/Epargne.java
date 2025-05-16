2e094824eb5de002ecb1ca9881c7ae01ae83439f
Mouhamed DIOP <mouhamed061100@outlook.fr>
2025-05-16 21:56:24 +0000

    Update Epargne.java

--- src/main/java/metier/Epargne.java
+++ src/main/java/metier/Epargne.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 public class Epargne {
-    private int idEpargne;
+    private int idEpargne; 
     private String nomEpargne;
     private Date dateCreation;
     private Date dateEcheance;
